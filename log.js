b728521cc70d39a07a7aa34d85371963ea8dc9e3
zanjs <root@zanjs.com>
2016-06-29 13:57:27 +0800

    :cat: init

---
+++ log.js
@@ -0,0 +1,49 @@
+/**
+* log logo	
+*/
+
+// (function(){
+
+	var logo = '%c\n####DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD####'+
+'\n##DDDDDDDDDDDDj          jDDDDDDDDDDDD##'+
+'\n#DDDDDDDDDDD,              ,DDDDDDDDDDD#'+
+'\n#DDDDDDDDD                    DDDDDDDDD#'+
+'\nDDDDDDDDD                      DDDDDDDDD'+
+'\nDDDDDDDt                        tDDDDDDD'+
+'\nDDDDDD                            DDDDDD'+
+'\nDDDD                                DDDD'+
+'\nDDD.     DDDDDD          DDDDDD     .DDD'+
+'\nDDD     DDDDDDDD:      :DDDDDDDD     DDD'+
+'\nDDt    GDDDj  :DD      DD:  fDDDG    jDD'+
+'\nDD     DDD   f  D.    DD  f   DDD     DD'+
+'\nDG    ,DDD f, E;DD    DGiE ;j DDD,    DD'+
+'\nD     iDDD E  GE.D    D EG  E DDDi     D'+
+'\nD      DDD EEE ELf    DfE EEE DDD      D'+
+'\nD      DDD  EEE D      D EEE  DDD      D'+
+'\nD       DDD    DD      DD    DDD       D'+
+'\nD       :DDDDDDD        DDDDDDD.       D'+
+'\nD    ,,,, LDDD     DD     DDDL ,,,,    D'+
+'\nD   ,,,,,,        :DD:        ,,,,,,   D'+
+'\nGD   ,,,,:       %cPanLi%c         :,,,,  DG'+
+'\ntDt   ,,                        ,,   tDt'+
+'\nttDD                                DDtt'+
+'\ntttDD;                            ;DDttt'+
+'\ntttttDDD                        DDDttttt'+
+'\nttttttttDDDDt              tDDDDtitttttt'+
+'\ntttttttttttDDDDDDDDDDDDDDDDDDtttt tttttt'+
+'\nttttt     ttttttttttttttttttttttttt tttt'+
+'\nttttt      ttttttttttttttttt  ttittt  tt'+
+'\nttttt  tt  ,tttttttttttttttt  t   t tttt'+
+'\nttttt  tt  tt   .  t      tt  t ttt tttt'+
+'\nttttt      t       t      tt  t tti tttt'+
+'\nttttt    :tt  tti  t  tt  tt  tt   ttttt'+
+'\njtttt  ttttt  ttt  t  tt  tt  tt   ttttf'+
+'\n#tttt  ttttt       t  tt  tt  tt   tttt#'+
+'\n#Lttt  tttttt   t  t  tt  tt  tt   ttt##'+
+'\n##Ltttttttttttttttttttttttttttttttttt###'+
+'\n####fttttttttttttttttttttttttttttttf####';
+
+console.log(logo,"font-size: 8pt;letter-spacing: 4px;line-height: 8pt;color:blue","color: #f00","color: #000")
+
+
+// })()
